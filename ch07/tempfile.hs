c67632583f074a6844b757e804715493f9124ec1
Will Salisbury <wsalisbury@apple.com>
2014-03-03 19:59:47 -0800

    Finally got tempfile.hs running.
    Turns out a) I had tab / space issues 🐑・・・・🔫 and b) I had to type the exception that i was 'catch'ing. I figure that the failure will be a 'NoMethodError' on Windoz platforms so that's what I've gone for.

--- ch07/tempfile.hs
+++ ch07/tempfile.hs
@@ -1,9 +1,9 @@
 -- file ch07/tempfile.hs
 
-import System.IO
+import System.IO(Handle, hGetContents, hTell, hSeek, hClose, hPutStrLn, openTempFile)
+import System.IO(SeekMode(AbsoluteSeek))
 import System.Directory(getTemporaryDirectory, removeFile)
-import Control.Exception(catch)
-import Control.Exception(finally)
+import Control.Exception(catch, finally, NoMethodError)
 
 -- The main entry point. Work with a temp file in myAction.
 main :: IO ()
@@ -15,43 +15,42 @@ file. When this function exits, that file will be closed and deleted
 because myAction was called from withTempFile -}
 myAction :: FilePath -> Handle -> IO ()
 myAction tempname temph =
-	do -- Start by displaying a greeting on the terminal
-		putStrLn "Welcome to tempfile.hs"
-		putStrLn $ "I have atemprary file at: " ++ tempname
+    do -- Start by displaying a greeting on the terminal
+        putStrLn "Welcome to tempfile.hs"
+        putStrLn $ "I have atemprary file at: " ++ tempname
 
-		-- Let's see what the initial position is
-		pos <- hTell temph
-		putStrLn $ "My initial position is: " ++ show pos
+        -- Let's see what the initial position is
+        pos <- hTell temph
+        putStrLn $ "My initial position is: " ++ show pos
 
-		-- Now, write some data to the temporary file
-		let tempdata = show [1..10]
-		putStrLn $ "Writing on line containing " ++ show (length tempdata) ++ " bytes: " ++ tempdata
-		hPutStrLn temph tempdata
+        -- Now, write some data to the temporary file
+        let tempdata = show [1..10]
+        putStrLn $ "Writing on line containing " ++ show (length tempdata) ++ " bytes: " ++ tempdata
+        hPutStrLn temph tempdata
 
-		{- Get our new position. This doesn't actually modify pos
-		in memory, but makes hte name "pos" correspond to a different
-		value for the remainder of the "do" block. -}
-		pos <- hTell temph
-		putStrLn $ "After writing, my new position is " ++ show pos
+        {- Get our new position. This doesn't actually modify pos
+        in memory, but makes hte name "pos" correspond to a different
+        value for the remainder of the "do" block. -}
+        pos <- hTell temph
+        putStrLn $ "After writing, my new position is " ++ show pos
 
-		-- Seek o the beginning of the file and display it
-		putStrLn $ "The file content is: "
-		hSeek temph AbsoluteSeek 0
+        -- Seek o the beginning of the file and display it
+        putStrLn $ "The file content is: "
+        hSeek temph AbsoluteSeek 0
 
-		-- hGetContents performs a lazy read and display
-		c <- hGetContents temph
-		putStrLn c
+        -- hGetContents performs a lazy read and display
+        c <- hGetContents temph
+        putStrLn c
 
-		putStrLn $ "Which could be expressed as this Haskell literal: "
-		print c
+        putStrLn $ "Which could be expressed as this Haskell literal: "
+        print c
 
 withTempFile :: String -> (FilePath -> Handle -> IO a) -> IO a
 withTempFile  pattern func =
-	do
-		tempdir <- catch (getTemporaryDirectory) (\_ -> return ".")
-		(tempfile, temph) <- openTempFile tempdir pattern
+    do
+        tempdir <- catch (getTemporaryDirectory) ( (\_ -> return ".") :: NoMethodError -> IO [Char])
+        (tempfile, temph) <- openTempFile tempdir pattern
 
-		finally (func tempfile temph)
-				(do hClose temph
-					removeFile tempfile)
-
+        finally (func tempfile temph)
+               (do hClose temph
+                   removeFile tempfile)
